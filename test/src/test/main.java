8d31bbc9431cc34d7ea5785766d3d61a00d57c74
David <dima.david1@gmail.com>
2015-03-31 15:58:28 +0300

    third change

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -6,7 +6,9 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		System.out.println("modificare remote");
+		System.out.println("Testing the branching model");
+		System.out.println("This is a new line");
 	}
 
 }
